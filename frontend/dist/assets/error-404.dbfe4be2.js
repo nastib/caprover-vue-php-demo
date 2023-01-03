dc7622bbb9040250001d235a3e0fbc990d210815
nastib <nastib@outlook.com>
2023-01-03 14:58:54 +0100

    three commit

---
+++ frontend/dist/assets/error-404.dbfe4be2.js
@@ -0,0 +1,3 @@
+/*! Built using the PremoWeb Software Development Kit for Vue 3 + PHP. Learn more at https://premoweb.com/sdk. */
+  
+import{_ as o,o as t,c as n,F as s,a as e}from"./index.f233b16a.js";const c={},r=e("h4",null,"Not found",-1),a=e("p",null,"The page does not exist.",-1);function _(l,d){return t(),n(s,null,[r,a],64)}const u=o(c,[["render",_]]);export{u as default};
---
+++ frontend/dist/assets/front-page.0648808b.js
@@ -0,0 +1,3 @@
+/*! Built using the PremoWeb Software Development Kit for Vue 3 + PHP. Learn more at https://premoweb.com/sdk. */
+  
+import{_ as e,o as t,c}from"./index.f233b16a.js";const n={};function o(r,s){return t(),c("p",null,"This is a test page....")}const _=e(n,[["render",o]]);export{_ as default};
